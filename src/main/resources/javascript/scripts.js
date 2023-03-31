87dc68073a3ce4a0ef02d9c4ce09f846828d62fe
JoelCunningham <s3949308@student.rmit.edu.au>
2023-03-31 11:48:32 +1100

    Moved javascript to a separate file

--- src/main/resources/javascript/scripts.js
+++ src/main/resources/javascript/scripts.js
@@ -1,8 +1,15 @@
 function showContent(id) {
+    // Open section
     var contents = document.querySelectorAll('.content');
     for (var i = 0; i < contents.length; i++) {
         contents[i].classList.remove('active');
     }
-    var content = document.getElementById(id);
-    content.classList.add('active');
+    document.getElementById(id).classList.add('active');
+
+    // Colour selected anchor
+    var anchors = document.querySelectorAll(".menu_options a");
+    for (var i = 0; i < anchors.length; i++) {
+        anchors[i].classList.remove("btn_selected");
+    }
+    document.getElementById("btn_" + id).classList.add("btn_selected");
 }
