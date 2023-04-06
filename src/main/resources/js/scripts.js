4784649a31c1db400f7613b34209d2d7d47e9a8e
JoelCunningham <s3949308@student.rmit.edu.au>
2023-04-06 20:46:50 +1000

    Begun work on front end for budget display

--- src/main/resources/js/scripts.js
+++ src/main/resources/js/scripts.js
@@ -6,6 +6,7 @@ window.onload = function() {
   }
 
   makeColumnsReadonly();
+  makeColumnsHidden();
 }
 
 // Code for menu selector buttons
@@ -35,12 +36,44 @@ function tempChangeText(button, text) {
 
 // Code to make fist and last table columns read only
 function makeColumnsReadonly() {
-  var table = document.querySelector('tbody');
-  var rows = table.querySelectorAll('tr');
-  for (var i = 0; i < rows.length; i++) {
-      var first_cell = rows[i].querySelector('td:first-child');
-      var last_cell = rows[i].querySelector('td:last-child');
-      first_cell.querySelector('input').setAttribute('readonly', true);
-      last_cell.querySelector('input').setAttribute('readonly', true);
+  var tables = document.querySelectorAll('tbody');
+  for (var t = 0; t < tables.length; t++) {
+    var table = tables[t];
+    var rows = table.querySelectorAll('tr');
+    for (var i = 0; i < rows.length; i++) {
+      var cells = rows[i].querySelectorAll('td');
+      for (var j = 0; j < 3; j++) {
+        cells[j].querySelector('input').setAttribute('readonly', true);
+      }
+      cells[15].querySelector('input').setAttribute('readonly', true);
+    }
   }
+}
+
+// Code to hide the first two columns of all tables
+function makeColumnsHidden() {
+  
+  // Code to hide the head
+  var table_heads = document.querySelectorAll('thead');
+  for (var t = 0; t < table_heads.length; t++) {
+    var head = table_heads[t];
+    var cell = head.querySelectorAll('th');
+    for (var i = 0; i < 2; i++) {
+      cell[i].classList.add('hidden');
+    }
+  }
+
+  // Code to hide the data
+  var table_bodies = document.querySelectorAll('tbody');
+  for (var t = 0; t < table_bodies.length; t++) {
+    var body = table_bodies[t];
+    var rows = body.querySelectorAll('tr');
+    for (var i = 0; i < rows.length; i++) {
+      var cells = rows[i].querySelectorAll('td');
+      for (var j = 0; j < 2; j++) {
+        cells[j].classList.add('hidden');
+      }
+    }
+  }
+  
 }
