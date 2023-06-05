4d8effacd77d0a50dea871b2edcfa215b2a5e883
JoelCunningham <s3949308@student.rmit.edu.au>
2023-06-06 01:08:28 +1000

    Wrote data structure for statements

---
+++ src/main/java/com/financemanager/type/Statement.java
@@ -0,0 +1,85 @@
+package com.financemanager.type;
+
+import java.time.LocalDate;
+
+import com.financemanager.Helper;
+import com.financemanager.JDBC;
+
+/**
+ * The Budget class represents a budget
+ */
+public class Statement {
+    public int month;
+    public int year;
+    public StatementItem[] items;
+
+    /**
+     * Constructs a new Statement object for a given month
+     * 
+     * @param month The month covered by the statement
+     * @param year The year of the month
+     */
+    public Statement(int month, int year) {
+        this.month = month;
+        this.year = year;
+        load();
+    }
+
+    /**
+     * Constructs a new Statement object for a given year
+     * 
+     * @param year The year covered by the statement
+     */
+    public Statement(int year) {
+        this.year = year;
+        this.month = -1;
+        load();
+    }
+
+    /**
+     * Loads statement items from the specified year into the object
+     *
+     */
+    public void load() {
+        JDBC jdbc = new JDBC();
+        this.items = jdbc.getStatementItems(this.year, this.month);
+    }
+
+    /**
+     * Finds the week value of a Statement
+     * 
+     * @param category_id The id of the category of the statement item
+     * @param week The week to compute
+     * 
+     * @return The total for the category in the given week
+     */
+    public float getWeekValue(int category_id, int week) {
+        float total = 0;
+        for (StatementItem item : items) {
+            LocalDate date = Helper.stringToDate(item.date);
+            if (item.category_id == category_id && date.getDayOfMonth() / 7 + 1 == week) {
+                total += item.amount;
+            }
+        }    
+        return total;
+    }
+
+    /**
+     * Finds the month value of a Statement
+     *
+     * @param category_id The id of the category of the statement item
+     * @param week The month to compute
+     * 
+     * @return The total for the category in the given month
+     */
+    public float getMonthValue(int category_id, int month) {
+        float total = 0;
+        for (StatementItem item : items) {
+            LocalDate date = Helper.stringToDate(item.date);
+            if (date.getMonthValue() == month) {
+                total += item.amount;
+            }
+        }    
+        return total;
+    }
+}
---
+++ src/main/java/com/financemanager/type/StatementItem.java
@@ -0,0 +1,27 @@
+package com.financemanager.type;
+
+/**
+ * The StatementItem class represents a entry in a statement
+ */
+public class StatementItem {
+
+    public int category_id; 
+    public float amount;
+    public String details;
+    public String date;
+
+    /**
+     * Constructs a new StatementItem object with the given category, year, month and amount
+     *
+     * @param category_id The id of the category of the statement item
+     * @param amount The amount of the statement item
+     * @param details The details of the statement item
+     * @param date The date the statement item occurred
+     */
+    public StatementItem(int category_id, float amount, String details, String date) {
+        this.category_id = category_id;
+        this.amount = amount;
+        this.details = details;
+        this.date = date;
+    }
+}
