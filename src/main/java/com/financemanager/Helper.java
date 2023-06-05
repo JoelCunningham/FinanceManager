4127612ce8c1284ddcd2407cabe343cb58a50741
JoelCunningham <s3949308@student.rmit.edu.au>
2023-06-05 17:38:06 +1000

    Wrote monthToInt helper method

--- src/main/java/com/financemanager/Helper.java
+++ src/main/java/com/financemanager/Helper.java
@@ -3,13 +3,31 @@ package com.financemanager;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Helper {
+
+    private static Map<String, Integer> monthMap = new HashMap<>();
+
+    static {
+        monthMap.put("January", 0);
+        monthMap.put("February", 1);
+        monthMap.put("March", 2);
+        monthMap.put("April", 3);
+        monthMap.put("May", 4);
+        monthMap.put("June", 5);
+        monthMap.put("July", 6);
+        monthMap.put("August", 7);
+        monthMap.put("September", 8);
+        monthMap.put("October", 9);
+        monthMap.put("November", 10);
+        monthMap.put("December", 11);
+    }
     
     public static String getGitVersion() throws IOException{
-
         InputStream inputStream = Runtime.getRuntime().exec("git describe --abbrev=7 --always  --long --match v* main").getInputStream();
         try (Scanner s = new Scanner(inputStream).useDelimiter("\\A")) {
             String result = s.hasNext() ? s.next() : null;
@@ -28,7 +46,6 @@ public class Helper {
     }
 
     public static void verifyDatabase(JDBC jdbc) {
-
         int curr_year = Calendar.getInstance().get(Calendar.YEAR);
         int max_year = jdbc.getMaxYear();
 
@@ -45,13 +62,15 @@ public class Helper {
           return Float.parseFloat(s);
     }
 
+    public static int monthToInt(String month) {
+        return monthMap.get(month);
+    }
+
     @Deprecated
     public static String[][] convertListToArray(List<String> list, int row_count, int col_count) {
-
-        String[][] array = new String[row_count][col_count];
-        
+        String[][] array = new String[row_count][col_count]; 
+       
         for (int i = 0; i < row_count; i++) {
-            
             for (int j = 0; j < col_count; j++) {
                 
                 int index = i * col_count + j;
