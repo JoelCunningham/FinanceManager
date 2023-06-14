389895733ba06fb9edab77491e28515bdea068dd
JoelCunningham <s3949308@student.rmit.edu.au>
2023-06-14 18:37:45 +1000

    Made Dropdown class formatting consistent

--- src/main/java/com/financemanager/item/Dropdown.java
+++ src/main/java/com/financemanager/item/Dropdown.java
@@ -14,12 +14,12 @@ public abstract class Dropdown {
     protected String name;
 
     /**
-     * Constructor for the Dropdown class.
+     * Constructor for the Dropdown class
      *
-     * @param context The context in which the Dropdown is being used.
-     * @param model   The model to which the Dropdown will add data.
-     * @param jdbc    The JDBC object used to retrieve data from the database.
-     * @param name    The name of the Dropdown.
+     * @param context The context in which the Dropdown is being used
+     * @param model   The model to which the Dropdown will add data
+     * @param jdbc    The JDBC object used to retrieve data from the database
+     * @param name    The name of the Dropdown
      */
     public Dropdown(Context context, Map<String, Object> model, JDBC jdbc, String name) {
         this.context = context;
@@ -29,7 +29,7 @@ public abstract class Dropdown {
     }
 
     /**
-     * Loads data into the Dropdown.
+     * Loads data into the Dropdown
      */
     public abstract int load();
 }
