e3a93d4cdc79b0c6774030c1c0171208178a0665
JoelCunningham <s3949308@student.rmit.edu.au>
2023-06-22 21:24:29 +1000

    Added id member to statement item class

--- src/main/java/com/financemanager/type/StatementItem.java
+++ src/main/java/com/financemanager/type/StatementItem.java
@@ -7,13 +7,31 @@ import com.financemanager.Helper;
  */
 public class StatementItem implements Comparable<StatementItem>{
 
+    public int id;
     public int category_id; 
     public float amount;
     public String details;
     public String date;
 
     /**
-     * Constructs a new StatementItem object with the given category, year, month and amount
+     * Constructs a new StatementItem object with given fields
+     *
+     * @param id The id of the statement item
+     * @param category_id The id of the category of the statement item
+     * @param amount The amount of the statement item
+     * @param details The details of the statement item
+     * @param date The date the statement item occurred
+     */
+    public StatementItem(int id, int category_id, float amount, String details, String date) {
+        this.id = id;
+        this.category_id = category_id;
+        this.amount = amount;
+        this.details = details;
+        this.date = date;
+    }
+
+    /**
+     * Constructs a new StatementItem object without an id
      *
      * @param category_id The id of the category of the statement item
      * @param amount The amount of the statement item
@@ -21,6 +39,7 @@ public class StatementItem implements Comparable<StatementItem>{
      * @param date The date the statement item occurred
      */
     public StatementItem(int category_id, float amount, String details, String date) {
+        this.id = -1;
         this.category_id = category_id;
         this.amount = amount;
         this.details = details;
